a9d103c9c9eb6436c44bf063fa0713a2f8f0e833
ipanteleiciuc <ionpanteleiciuc@gmail.com>
2022-12-29 00:08:11 +0100

    back to old structure

--- app/order-service/src/main/java/com/iontech/orderservice/controller/OrderController.java
+++ app/order-service/src/main/java/com/iontech/orderservice/controller/OrderController.java
@@ -19,7 +19,7 @@ public class OrderController {
     @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
-        return "ORder placed successfully";
+        return "Order placed successfully";
     }
 
     
