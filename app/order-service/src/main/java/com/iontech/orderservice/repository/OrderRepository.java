7230ff48ccd0260204a9b9d1fc5a11bf4d32f34f
Ion PANTELEICIUC <74062814+IonPanteleiciuc@users.noreply.github.com>
2022-12-29 00:11:31 +0100

    Delete app directory

--- app/order-service/src/main/java/com/iontech/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.iontech.orderservice.repository;
-
-import com.iontech.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- app/product-service/src/main/java/com/iontech/productservice/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.iontech.productservice.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.iontech.productservice.model.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String>{
-    
-}
